be8870fbe12645c4852a64b9b360c2ee69b3dd25
Антон Фролов <fr.anton@mail.ru>
2026-02-27 10:24:04 +0300

    Example app: migrate to native fetch

--- example/src/utils/request/request.ts
+++ example/src/utils/request/request.ts
@@ -1,12 +1,13 @@
-import superagent from 'superagent';
-
-import type { Response } from 'superagent';
-
-type TGetResponseBody = (response: Response) => any;
 type TRequest = (url: string) => Promise<any>;
 
-const getResponseBody: TGetResponseBody = (response) => response && response.body;
-
-const request: TRequest = (url) => superagent.get(`${__HOST__}${url}`).then(getResponseBody);
+const request: TRequest = async (url) => {
+    try {
+        const response = await fetch(`${__HOST__}${url}`);
+        !response.ok && console.error(`HTTP error! Status: ${response.status}`);
+        return await response.json();
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+};
 
 export default request;
